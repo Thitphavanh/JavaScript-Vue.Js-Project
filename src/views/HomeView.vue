7720f99d2dbfd14198911c1249eb6f64584cbdf3
Thitphavanh <thitphavanh@gmail.com>
2023-04-23 17:34:40 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const title = ref('My Counter App')
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+
+<template>
+  <div class="counter">
+    <h1>{{ title }}</h1>
+    <button class="btn-counter">++</button>
+    <button @click="increment" class="btn-counter">+</button>
+    {{ count }}
+    <button @click="decrement" class="btn-counter">-</button>
+    <button class="btn-counter">--</button>
+  </div>
+  <div>
+    <input type="text" name="title-model" value="" />
+    <input type="number" name="count-model" value="" />
+  </div>
+</template>
+
+<style scoped>
+.counter,
+.counter button {
+  font-size: 2rem;
+}
+.counter {
+  text-align: center;
+}
+.btn-counter {
+  margin: 3px;
+}
+
+input {
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 5px;
+}
+</style>
